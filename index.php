5241f5326c7485c4fbb1ef564698e0a3b4b3eb03
Serafín Piña <arespin23@gmail.com>
2022-02-06 11:11:38 +0100

    First commint

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Actividad 1.1</h3>
+    <?php
+        $valor = 5;
+
+        $dolares = 1.14 * $valor;
+
+        echo $dolares;
+        define("DOLARES", $dolares);
+
+    ?>
+    <span>--- Fin actividad 1.1 ---</span>
+</body>
+</html>
