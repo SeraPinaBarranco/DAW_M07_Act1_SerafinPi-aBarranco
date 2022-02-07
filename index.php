bdf574d11f922d4a492e6f9ee51c590c3c0d421a
Serafín Piña <arespin23@gmail.com>
2022-02-07 21:18:42 +0100

    Inicio de arrays

--- index.php
+++ index.php
@@ -59,14 +59,13 @@
                 //descomponer la ecuacion en partes
                 $bCuadrado =  pow($b,2);
                 $discriminante = $bCuadrado -4 * $a * $c;
-                //echo($discriminante);
                 
                 if($discriminante >= 0){//Si lo que hay en el discriminante no es negativo
                     
                     $numeradorMas = (-$b + sqrt($discriminante))/ (2 * $a);
                     $numeradorMenos = (-$b - sqrt($discriminante))/ (2 * $a);
 
-                    if($numeradorMas == $numeradorMenos){
+                    if($numeradorMas == $numeradorMenos){//Si los resultados son iguales solo hay una solucion posible
                         echo("Las unica solución es " . $numeradorMas);
                     }else{
                         echo("Las soluciones a la ecuación son " . $numeradorMas . " y " . $numeradorMenos);
@@ -77,8 +76,6 @@
                     exit("El discriminante es negativo, no se puede solucionar");
                 }
                 
-                //echo("Las soluciones a la ecuación son " . $resMas . " y " . $resMenos);
-                //echo($raiz);
            }
            
            
@@ -87,6 +84,59 @@
     <br>
     <h3>--- Fin actividad 1.3 ---</h3>
 
+    <h3>Actividad 1.4</h3>
+           <?php 
+                //Hayar longitud de la circunferencia
+                // L = 2pi * r
+                $r= 6;
+
+                $longCircunferencia= 2 * M_PI * $r;
+
+                echo("La longitud de la circunferencia es: " . $longCircunferencia);
+                echo("<br>");
+                //Hayar el area de la circunferencia
+                //A = PI*r^2
+                $area = M_PI * pow($r,2);
+                echo("El area de la circunferencia es: " . $area);
+                echo("<br>");
+                //Hayar el volumen de la circunferencia
+                //V = 3/4PI * r^3
+                $volumen= 4/3 * M_PI * pow($r,3);
+                echo("El volumen de la esfera es: " . $volumen);
+
+           ?>
+    <br>
+    <h3>--- Fin actividad 1.4 ---</h3>
+
+    <h3>Actividad 1.5</h3>
+           <?php 
+               $miArray = array();
+
+               echo("<table >");
+               echo("<tr >");
+               for ($i=0; $i < 20; $i++) { 
+                   $miArray[$i]= $i + 1;
+
+                   echo("<td style='border: 1px solid black'>" . $miArray[$i] . "<td>");
+
+                   for ($j=0; $j < 20; $j++) { 
+                        echo("<td style='border: 1px solid black'>" . $miArray[$i][$j] = $j +1  . "<td>");
+                   }
+                   /*<table>
+                        
+                        <tr>
+                            <td>Alfreds Futterkiste</td>
+                            <td>Maria Anders</td>
+                            <td>Germany</td>
+                        </tr>
+                        
+                    </table>*/
+               }
+               echo($miArray);
+               echo("</tr><table>");
+           ?>
+    <br>
+    <h3>--- Fin actividad 1.5 ---</h3>
 
 </body>
 </html>
